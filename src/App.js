0b88ee51f9ae0d38c0da85f8619403b4a648912f
Coffey May <jcoffeymay6@gmail.com>
2021-05-05 12:19:48 -0500

    m

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Navbar, Products, Cart, Checkout } from './components';
 import { commerce } from './lib/commerce';
-//APP
+
 const App = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [products, setProducts] = useState([]);
